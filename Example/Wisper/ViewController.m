06362e7d82fa18f076d857e7db4b52b29daa1f18
Patrik Nyblad <patrik@Patrik-Nyblads-MacBook-Pro.local>
2015-08-03 16:09:26 +0200

    Add example project

---
+++ Example/Wisper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Wisper
+//
+//  Created by Patrik Nyblad on 03/08/15.
+//  Copyright (c) 2015 Widespace AB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
