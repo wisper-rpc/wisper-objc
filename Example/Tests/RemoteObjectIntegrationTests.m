840b66e2e5e42ca2fef17fe0224ed595d2a25870
Patrik Nyblad <patrik@Patrik-Nyblads-Macbook-Pro.local>
2016-02-18 12:47:20 +0100

    Create integration test class and test registering class

---
+++ Example/Tests/RemoteObjectIntegrationTests.m
@@ -0,0 +1,50 @@
+//
+//  RemoteObjectIntegrationTests.m
+//  Wisper
+//
+//  Created by Patrik Nyblad on 17/02/16.
+//  Copyright © 2016 Patrik Nyblad. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+#import "WSPRTestObject.h"
+#import "WSPRGatewayRouter.h"
+#import "WSPRClassRouter.h"
+
+@interface RemoteObjectIntegrationTests : XCTestCase
+
+@property (nonatomic, strong) WSPRGatewayRouter *gatewayRouter;
+
+@end
+
+@implementation RemoteObjectIntegrationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.gatewayRouter = [[WSPRGatewayRouter alloc] init];
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testRegisteringTestObject
+{
+    [_gatewayRouter exposeRoute:[WSPRClassRouter routerWithClass:[WSPRTestObject class]] onPath:@"wisp.test.TestObject"];
+
+    WSPRNotification *notification = [[WSPRNotification alloc] init];
+    notification.method = @"wisp.test.TestObject.append";
+    notification.params = @[@"Hello ", @"world!"];
+    
+    WSPRRouter *wispRouter = _gatewayRouter.routes[@"wisp"];
+    WSPRRouter *testRouter = wispRouter.routes[@"test"];
+    WSPRClassRouter *testObjectRouter = testRouter.routes[@"TestObject"];
+    
+    XCTAssertEqual(testObjectRouter.classModel.classRef, [WSPRTestObject class], @"Test Object not registered properly!");
+}
+
+
+@end
