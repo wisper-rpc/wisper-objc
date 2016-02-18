09c727a34731e86c7d06ebb44da519018990e920
Patrik Nyblad <patrik@Patrik-Nyblads-Macbook-Pro.local>
2016-02-18 15:07:59 +0100

    Add tests for notifying and requesting static methods

--- Example/Tests/RemoteObjectIntegrationTests.m
+++ Example/Tests/RemoteObjectIntegrationTests.m
@@ -42,16 +42,44 @@
     XCTAssertEqual(testObjectRouter.classModel.classRef, [WSPRTestObject class], @"Test Object not registered properly!");
 }
 
+- (void)testNotifyStaticMethod
+{
+    id testObjectClassMock = OCMClassMock([WSPRTestObject class]);
+    
+    OCMExpect(ClassMethod([testObjectClassMock appendString:[OCMArg checkWithBlock:^BOOL(id obj) {
+        return [(NSString *)obj isEqualToString:@"Hello "];
+    }] withString:[OCMArg checkWithBlock:^BOOL(id obj) {
+        return [(NSString *)obj isEqualToString:@"world!"];
+    }]]));
+    
+    [_gatewayRouter exposeRoute:[WSPRClassRouter routerWithClass:[WSPRTestObject class]] onPath:@"wisp.test.TestObject"];
+    
     WSPRNotification *notification = [[WSPRNotification alloc] init];
     notification.method = @"wisp.test.TestObject.append";
     notification.params = @[@"Hello ", @"world!"];
     
-    WSPRRouter *wispRouter = _gatewayRouter.routes[@"wisp"];
-    WSPRRouter *testRouter = wispRouter.routes[@"test"];
-    WSPRClassRouter *testObjectRouter = testRouter.routes[@"TestObject"];
+    [_gatewayRouter.gateway handleMessage:notification];
     
-    XCTAssertEqual(testObjectRouter.classModel.classRef, [WSPRTestObject class], @"Test Object not registered properly!");
+    OCMVerifyAll(testObjectClassMock);
 }
 
+- (void)testRequestStaticMethod
+{
+    XCTestExpectation *expectation = [self expectationWithDescription:@"correct response"];
+    
+    [_gatewayRouter exposeRoute:[WSPRClassRouter routerWithClass:[WSPRTestObject class]] onPath:@"wisp.test.TestObject"];
+    
+    WSPRRequest *request = [[WSPRRequest alloc] init];
+    request.method = @"wisp.test.TestObject.append";
+    request.params = @[@"Hello ", @"world!"];
+    request.responseBlock = ^(WSPRResponse *response){
+        if ([(NSString *)response.result isEqualToString:@"Hello world!"])
+            [expectation fulfill];
+    };
+    
+    [_gatewayRouter.gateway handleMessage:request];
+    
+    [self waitForExpectationsWithTimeout:0.1 handler:nil];
+}
 
 @end
