db8e23403f88d68bde16951815f23766ffe3a0b6
Patrik Nyblad <patrik.nyblad@widespace.com>
2016-03-30 17:54:28 +0200

    Add Wisper.h file to make it easier for developers to import Wisper headers

---
+++ Pod/Classes/Wisper.h
@@ -0,0 +1,33 @@
+//
+//  Wisper.h
+//  Pods
+//
+//  Created by Patrik Nyblad on 24/03/16.
+//
+
+#import <Wisper/WSPRException.h>
+#import <Wisper/WSPRExceptionHandler.h>
+#import <Wisper/WSPRGateway.h>
+#import <Wisper/WSPRErrorMessage.h>
+#import <Wisper/WSPRMessage.h>
+#import <Wisper/WSPRMessageFactory.h>
+#import <Wisper/WSPRNotification.h>
+#import <Wisper/WSPRRequest.h>
+#import <Wisper/WSPRResponse.h>
+#import <Wisper/WSPRError.h>
+#import <Wisper/WSPREvent.h>
+#import <Wisper/WSPRHelper.h>
+#import <Wisper/WSPRObject.h>
+#import <Wisper/WSPRRemoteObject.h>
+#import <Wisper/WSPRTestObject.h>
+#import <Wisper/WSPRViewController.h>
+#import <Wisper/WSPRClass.h>
+#import <Wisper/WSPRClassInstance.h>
+#import <Wisper/WSPRClassMethod.h>
+#import <Wisper/WSPRClassProperty.h>
+#import <Wisper/WSPRRemoteObjectEventFunctionRouter.h>
+#import <Wisper/WSPRClassRouter.h>
+#import <Wisper/WSPRFunctionRouter.h>
+#import <Wisper/WSPRGatewayRouter.h>
+#import <Wisper/WSPRInstanceRegistry.h>
+#import <Wisper/WSPRRouter.h>
